2086cf9507ee713b3a40bff19f83b39e7f97d268
Roberto <rvallejosbarria@gmail.com>
2022-04-18 18:01:04 -0400

    Heap Sort

--- src/test/java/com/example/sorting/SortTest.java
+++ src/test/java/com/example/sorting/SortTest.java
@@ -6,9 +6,22 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SortTest {
     @Test
-    public void should_sort_elements() {
+    public void should_sort_elements_using_bubble_sort() {
         // given
         Sort sort = new BubbleSort();
+        int[] input = new int[]{4, 5, 1, 2, 10, 12, 1};
+
+        // when
+        sort.sort(input);
+
+        // then
+        assertThat(input).isEqualTo(new int[]{1, 1, 2, 4, 5, 10, 12});
+    }
+
+    @Test
+    public void should_sort_elements_using_heap_sort() {
+        // given
+        Sort sort = new HeapSort();
         int[] input = new int[]{4, 5, 1, 2, 10, 12, 1};
 
         // when
